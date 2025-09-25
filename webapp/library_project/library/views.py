87770516e6ecc9a49f36e81fd3e73a5319dc4182
samod <83113845+BugSlayer9000@users.noreply.github.com>
2025-09-26 00:28:20 +0100

    Add book list view and template
    - Created book_list view to display all books
    - Added book_list.html template with loop to render titles and authors
    - Integrated view with model and confirmed data displays correctly
    - Verified admin adds books that appear on book list page

--- webapp/library_project/library/views.py
+++ webapp/library_project/library/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import book
 # Create your views here.
+
+def book_list(reqest):
+    books = book.objects.all()
+    return render(reqest, "library/book_list.html", {"books":books})
+
+
